ccec641d9467620ea5efce35eefda39b5e904fe3
A-STAR <andreysuperstar@gmail.com>
2019-07-04 12:22:25 +0300

    feat: add modular events

---
+++ modular-events/weekly.js
@@ -0,0 +1,9 @@
+const Job = require('./job');
+
+const job = new Job();
+
+job.on('done', function (details) {
+  console.log('Weekly email job was completed at', details.completedOn);
+});
+
+job.emit('start');
