f916855b4a7af7907eee33c117773610553bc522
A-STAR <andreysuperstar@gmail.com>
2019-10-23 22:35:15 +0300

    feat: add validation to REST API

--- rest-api/app.js
+++ rest-api/app.js
@@ -23,7 +23,23 @@ app.get('/profile', (request, response) => {
 });
 
 app.post('/profile', (request, response) => {
-  profiles.push(request.body);
+  if (!request.body.first_name.trim() || !request.body.last_name.trim()) {
+    return response.sendStatus(400);
+  }
+
+  if (request.body.last_name.length <= 1) {
+    return response.send({
+      error: 'Invalid format for last name.'
+    });
+  }
+
+  // whitelisting
+  const profile = {
+    first_name: request.body.first_name,
+    last_name: request.body.last_name
+  };
+
+  profiles.push(profile);
 
   console.log('created', profiles);
 
